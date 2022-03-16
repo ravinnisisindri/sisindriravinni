3b168d07602c688ca81263b88f33743aebace877
ravinnisisindri <96733202+ravinnisisindri@users.noreply.github.com>
2022-03-16 19:27:23 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,83 +1,42 @@
-package com.javaproject;
+package Problem_Statement9;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.util.HashMap;
 import java.util.Scanner;
-
-class file{
-    file()
-    {
-        try{
-        FileReader fr = new FileReader("output.txt");
-        String str = "";
-        int i;
-	    while ((i = fr.read()) != -1)str += (char)i;
-	    System.out.println("Data Stored in file : \n"+str);
-    }
-    catch(Exception e)
-    {
-        System.out.println(e);
-    }
-}
-}
-
+ 
 public class Main {
-	static void solve(){
-	    try{
-	        System.out.println("\tWELCOME");
-	        int option=-1,count=0;
-	        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	        FileWriter fw = new FileWriter("output.txt");
-	        while(option!=2)
-	        {
-	            count++;
-	            System.out.println("1.Add details\n2.Exit");
-	            option=Integer.parseInt(br.readLine());
-	               int age,rollNo;
-	               String Name,Address;
-	            if(option==1)
-	            {
-	                System.out.println("Enter Name :");
-	                Name=br.readLine();
-	                System.out.println("Enter Roll Number : ");
-	                rollNo=Integer.parseInt(br.readLine());
-	                System.out.println("Enter age : ");
-	                age=Integer.parseInt(br.readLine());
-	                System.out.println("Enter Address : ");
-	                Address=br.readLine();
-	                String details=String.valueOf(count)+"."+"Name : "+Name+"\n Roll Number : "+rollNo+"\n Age : "+age+"\n Address : "+Address+"\n";
-	                System.out.println("Do you want to enter details into file :\n1.Yes\n2.No");
-	                String o=br.readLine();
-	                if(o.equals("1"))
-	                {
-	                	fw.write(details);
-	                	System.out.println("Data Stored ");
-	                }
-	                else
-	                {
-	                    fw.close();
-	                    break;
-	                }
-	            }
-	        }
-	        System.out.println("Do you want view file data on terminal :\n1.Yes\n2.No");
-	        String temp=br.readLine();
-	        if(temp.equals("1"))
-	        new file();
-	    }
-	    catch(Exception e){
-	        System.out.println(e);
-	    }
-	    finally{
-	        System.out.println("Program Completed");
-	    }
-	}
-	public static void main(String[] args) {
-		solve();
-	}
+ 
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter length of array : ");
+        int[] arr = new int[sc.nextInt()];
+        System.out.println("enter array values : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            
+        }
+        HashMap<Integer, Integer> freqMap = solveIterative(arr);
+ 
+        for(int value : freqMap.keySet())
+        {
+            System.out.println(value + " occurs " + freqMap.get(value)+ " times");
+        }
+ 
+    }
+ 
+    public static HashMap<Integer, Integer> solveIterative(int[] arr)
+    {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        for(int value : arr)
+        {
+            if(!freqMap.containsKey(value))
+            {
+                freqMap.put(value, 1);
+            }
+            else {
+                freqMap.put(value, freqMap.get(value)+1);
+            }
+        }   
+        return freqMap;
+    }
 }
-
-
